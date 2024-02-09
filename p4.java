7ff13051740e7bef9e55f119f25920fca683cd5b
Powaleom <powaleom@gmail.com>
2024-02-10 01:47:18 +0530

    p4

---
+++ p4.java
@@ -0,0 +1,29 @@
+
+import java.lang.*;
+
+public class p4 {
+
+    
+    public static void sheela() {
+       System.out.println("Hi baby");
+    }
+    
+    public static void add()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Mb");
+        sheela();
+        add();
+        System.out.println("ME");
+    }
+    
+    
+    
+}
+
