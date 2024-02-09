218d2ac33d1ad4853c3a970f7b2d5d960ff06c6e
Powaleom <powaleom@gmail.com>
2024-02-10 01:56:23 +0530

    evenodd

---
+++ evenodd.java
@@ -0,0 +1,20 @@
+//
+import java.lang.*;
+
+public class evenorodd {
+
+    public static void find(int a)
+    {
+        if(a %2==0)
+        {
+            System.out.println("Entered number is even="+a);
+        }
+        else{
+            System.out.println("Enter number is not even="+a);
+        }
+    }
+    public static void main(String[] args) {
+        find(3);
+    }
+    
+}
