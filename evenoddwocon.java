add2b17c1d630d0e1df042ffa140982ebfe66ccd
Powaleom <powaleom@gmail.com>
2024-02-10 01:59:10 +0530

    evenoddwithcondition

---
+++ evenoddwocon.java
@@ -0,0 +1,13 @@
+
+import java.lang.*;
+
+public class evenoddwocon{
+    public static void find(int a)
+    {
+        System.out.println(a%2==0 ?"even"+a : "odd"+a);
+    }
+    public static void main(String[] args) {
+      find(2);
+    }
+    
+}
