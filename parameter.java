a46e93c60a282d8f95d78411662de6f863f5cd93
Powaleom <powaleom@gmail.com>
2024-02-10 01:53:52 +0530

    parameter

---
+++ parameter.java
@@ -0,0 +1,19 @@
+
+import java.lang.*;
+
+
+public class parameter{
+
+    public static void add(int a,int b)
+    {
+        int c=a+b;
+        System.out.println(c);
+    }
+   
+    public static void main(String[] args) {
+       add(10,20);
+       add(90,90);
+       
+    }
+    
+}
