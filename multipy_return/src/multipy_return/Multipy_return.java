07fedb544d23ca649da65135d74f7a1f8a4e12b3
Powaleom <powaleom@gmail.com>
2024-02-10 16:46:54 +0530

    return

---
+++ multipy_return/src/multipy_return/Multipy_return.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multipy_return;
+
+/**
+ *
+ * @author ompowale
+ */
+public class Multipy_return {
+
+    public static int multi(int a,int b,int c,int d){
+        int result=a*b*c*d;
+        return result;
+    }
+    public static void main(String[] args) {
+      System.out.println("mb");
+      System.out.println(multi(10,20,5,1));
+    }
+    
+}
